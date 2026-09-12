152fa199157cc6505d3b1b32c09efd3f15f7282c
Poojasri Reddy <poojasrikandhula@gmail.com>
2026-09-12 12:41:59 +0000

    feat: implement week 4 process execution

--- src/main.c
+++ src/main.c
@@ -1,40 +1,54 @@
-#define _POSIX_C_SOURCE 200809L
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int main(void) {
     char *line = NULL;
     size_t len = 0;
-    ssize_t nread;
+    char *args[64];
 
     while (1) {
         printf("shellforge$ ");
         fflush(stdout);
 
-        nread = getline(&line, &len, stdin);
-
-        if (nread == -1) {
-            printf("\nExiting cleanly...\n");
+        if (getline(&line, &len, stdin) == -1)
             break;
+
+        line[strcspn(line, "\n")] = '\0';
+
+        int i = 0;
+        char *token = strtok(line, " \t");
+
+        while (token != NULL && i < 63) {
+            args[i++] = token;
+            token = strtok(NULL, " \t");
         }
 
-        if (nread > 0 && line[nread - 1] == '\n') {
-            line[nread - 1] = '\0';
-        }
+        args[i] = NULL;
 
-        if (strcmp(line, "exit") == 0) {
+        if (i == 0)
+            continue;
+
+        if (strcmp(args[0], "exit") == 0)
             break;
-        }
 
-        if (strlen(line) > 0) {
-            printf("You typed: %s\n", line);
+        pid_t pid = fork();
+
+        if (pid == 0) {
+            execvp(args[0], args);
+            perror("Command execution error");
+            exit(1);
+        }
+        else if (pid > 0) {
+            waitpid(pid, NULL, 0);
+        }
+        else {
+            perror("Fork creation error");
         }
     }
 
     free(line);
-
     return 0;
 }

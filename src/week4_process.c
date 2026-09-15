7ac08df6373907a6e3260aea617774260882ca75
Poojasri Reddy <poojasrikandhula@gmail.com>
2026-09-15 07:22:46 +0000

    feat: add week 4 process execution

---
+++ src/week4_process.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(void) {
+    char *line = NULL;
+    size_t len = 0;
+    char *args[64];
+
+    while (1) {
+        printf("shellforge$ ");
+        fflush(stdout);
+
+        if (getline(&line, &len, stdin) == -1)
+            break;
+
+        line[strcspn(line, "\n")] = '\0';
+
+        int i = 0;
+        char *token = strtok(line, " \t");
+
+        while (token != NULL && i < 63) {
+            args[i++] = token;
+            token = strtok(NULL, " \t");
+        }
+
+        args[i] = NULL;
+
+        if (i == 0)
+            continue;
+
+        if (strcmp(args[0], "exit") == 0)
+            break;
+
+        pid_t pid = fork();
+
+        if (pid == 0) {
+            execvp(args[0], args);
+            perror("Execution error");
+            exit(1);
+        } else if (pid > 0) {
+            waitpid(pid, NULL, 0);
+        } else {
+            perror("Fork creation error");
+        }
+    }
+
+    free(line);
+    return 0;
+}
